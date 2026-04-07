d433019d52f3c5208f32e93aea6069bf2873b24a
osphvdhwj <manjukhatri0101@gmail.com>
2026-04-07 19:09:40 +0530

    Fix: Added Gradle configurations and wrapper generation step

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.yourname.nanodl"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.yourname.nanodl"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.github.TeamNewPipe:NewPipeExtractor:v0.24.2")
+    implementation("com.grack:nanojson:1.9")
+}
